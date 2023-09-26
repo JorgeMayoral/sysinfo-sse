e7be9d20f95cb0e9cde76549165c1d003ba50e5a
Jorge Mayoral Álvarez <jorge.ma@yorch.dev>
2023-09-26 18:13:23 +0200

    feat(frontend): :sparkles: add sse event and install tailwindcss

--- sysinfo-sse-frontend/src/App.tsx
+++ sysinfo-sse-frontend/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface SysInfo {
+	hostname: string;
+	uptime: number;
+	os: string;
+	loadOne: number;
+	loadFive: number;
+	loadFifteen: number;
 }
 
-export default App
+function App() {
+	const [sysinfo, setSysinfo] = useState<SysInfo>();
+
+	useEffect(() => {
+		const eventSource = new EventSource('http://localhost:8080/realtime');
+		eventSource.onmessage = (event) => {
+			const data = JSON.parse(event.data);
+			setSysinfo(data);
+			console.log(data);
+		};
+		eventSource.onerror = (event) => {
+			console.error(event);
+		};
+
+		return () => {
+			eventSource.close();
+		};
+	}, []);
+
+	return (
+		<main className="h-screen w-screen flex flex-col justify-center items-center">
+			<h1 className="font-bold text-center text-4xl underline">SysInfo SSE</h1>
+		</main>
+	);
+}
+
+export default App;
